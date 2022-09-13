fb719e9e62182c45e73bf07c78f224808cb34b9f
nsikakudoh <nsikak@sparknerds.io>
2022-09-13 15:20:35 -0600

    create about useState

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,11 @@
+import {useState, useEffect} from "react"
+
+
 function About(props) {
+  // create state to hold "about" data
+  const [about, setAbout] = useState();
+
+
     return <h1>About</h1>;
   }
   
